f89d2d86469eaf58cb2086e9b0563628159e4446
Geoffrey Ihsan <geoffreyihsan@geoffrey-4.local>
2023-12-22 14:09:55 +0700

    Test deployment on 22nd December 2023

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -54,26 +54,23 @@ export default function Header({
     <>
       
       <div className={cx("desktop-banner")}>
-        {/* <!-- Leaderboard --> */}
-        <div id="div-gpt-ad-1460363567691-1">
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `googletag.cmd.push(function() { googletag.display('div-gpt-ad-1460363567691-1'); });`,
-            }}
-          />
-        </div>
+        {`<!-- Leaderboard -->
+            <div class="desktop-leaderboard">
+            <div id='div-gpt-ad-1460363567691-1' style='height:90px; width:728px;'>
+            <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1460363567691-1'); });
+            </script>
+          </div>`}
       </div>
       <div className={cx("mobile-banner")}>
-        {/* <!-- Mobile Leaderboard --> */}
-        <div id="div-gpt-ad-1460363567691-2">
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `googletag.cmd.push(function() { googletag.display('div-gpt-ad-1460363567691-2'); });`,
-            }}
-          />
-        </div>
+        {`<!-- Mobile Leaderboard -->
+            <div class="mobile-leaderboard">
+            <div id='div-gpt-ad-1460363567691-2' style='height:50px; width:320px;'>
+            <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1460363567691-2'); });
+            </script>
+            </div>
+          </div>`}
       </div>
       {/* Main Header */}
       <header className={cx("component", { sticky: isScrolled })}>
--- components/Main/Main.js
+++ components/Main/Main.js
@@ -17,7 +17,37 @@ export default function Main({ children, className, ...props }) {
       {...props}
     >
       {children}
-      
+      <div className={cx("mobile-banner")}>
+        {/* Masthead Top Mobile */}
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- /6808792/DAMAN_MASTHEAD_BOTTOM_MOBILE -->
+            <div id='div-gpt-ad-1701941651336-0' style='min-width: 300px; min-height: 250px;'>
+              <script>
+                googletag.cmd.push(function() { googletag.display('div-gpt-ad-1701941651336-0'); });
+              </script>
+            </div>
+          `,
+          }}
+        />
+      </div>
+      <div className={cx("desktop-banner")}>
+        {/* Masthead Top Desktop */}
+        <div id="div-gpt-ad-1701941037105-0" />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+            <!-- /6808792/DAMAN_MASTHEAD_BOTTOM -->
+            <div id='div-gpt-ad-1701941453692-0' style='min-width: 970px; min-height: 250px;'>
+              <script>
+                googletag.cmd.push(function() { googletag.display('div-gpt-ad-1701941453692-0'); });
+              </script>
+            </div>
+          `,
+          }}
+        />
+      </div>
     </main>
   );
 }
