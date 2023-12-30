298a5c07eb0c906b8e26883b1eb5c12007faa982
Geoffrey Ihsan <geoffreyihsan@geoffrey-4.local>
2023-12-30 16:02:12 +0700

    Test deployment on 30th December 2023

--- components/ContentWrapper/ContentWrapper.js
+++ components/ContentWrapper/ContentWrapper.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import className from "classnames/bind";
 import styles from "./ContentWrapper.module.scss";
 import { Interscroller } from "../../components";
@@ -5,13 +6,29 @@ import { Interscroller } from "../../components";
 let cx = className.bind(styles);
 
 export default function ContentWrapper({ content, children, className }) {
+  const [shouldShowInterscroller, setShouldShowInterscroller] = useState(false);
+
+  useEffect(() => {
+    // Helper function to count paragraphs in the content
+    const countParagraphs = (html) => {
+      const div = document.createElement("div");
+      div.innerHTML = html;
+      const paragraphs = div.querySelectorAll("p");
+      return paragraphs.length;
+    };
+
+    // Determine if the Interscroller should be shown
+    const paragraphsCount = countParagraphs(content);
+    setShouldShowInterscroller(paragraphsCount >= 5);
+  }, [content]);
+
   return (
     <>
       <article className={cx(["component", className])}>
         <div dangerouslySetInnerHTML={{ __html: content ?? "" }} />
         {children}
       </article>
-      <Interscroller />
+      {shouldShowInterscroller && <Interscroller />}
     </>
   );
 }
