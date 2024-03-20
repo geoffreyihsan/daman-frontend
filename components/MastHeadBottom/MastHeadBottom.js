cee263bcb2b41b6d8095c4a4952a76e77ba60b68
DestinAsian <geoffreyihsan@geoffrey-4.local>
2024-03-20 16:28:54 +0700

    Production deployment on 20th March 2024

--- components/MastHeadBottom/MastHeadBottom.js
+++ components/MastHeadBottom/MastHeadBottom.js
@@ -6,27 +6,15 @@ let cx = classNames.bind(styles);
 
 export default function MastHeadBottom() {
   return (
-    <>
-      <div className={cx("mobile-banner")}>
-        {/* Masthead Top Mobile */}
-        <div className={cx("masthead-banner")}>
-          <Ad
-            adUnit="/6808792/DAMAN_MASTHEAD_BOTTOM_MOBILE"
-            name="div-gpt-ad-1704170813691-0"
-            size={[[320, 330], [300, 250]]}
-          />
-        </div>
+    <div className={cx("desktop-banner")}>
+      {/* Masthead Top Desktop */}
+      <div className={cx("masthead-banner")}>
+        <Ad
+          adUnit="/6808792/DAMAN_MASTHEAD_BOTTOM"
+          name="div-gpt-ad-1701941453692-0"
+          size={[970, 250]}
+        />
       </div>
-      <div className={cx("desktop-banner")}>
-        {/* Masthead Top Desktop */}
-        <div className={cx("masthead-banner")}>
-          <Ad
-            adUnit="/6808792/DAMAN_MASTHEAD_BOTTOM"
-            name="div-gpt-ad-1701941453692-0"
-            size={[970, 250]}
-          />
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
