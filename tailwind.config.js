718bca8e7b2806c9c13d50596aea67a1596ede26
DestinAsian <geoffreyihsan@geoffrey-4.local>
2024-07-03 13:15:48 +0700

    Production deployment on 3rd of July 2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-const {nextui} = require('@nextui-org/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./wp-templates/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(select|listbox|divider|popover|button|ripple|spinner|scroll-shadow).js"
   ],
   theme: {
     extend: {},
